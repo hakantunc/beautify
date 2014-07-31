b9b3f3e7989da44af2ed6c59c3074b90d8c3b0f5
Hakan Tunc <hakan.tunc@vanderbilt.edu>
2014-07-31 11:49:08 -0500

    Create options.json file

--- beautify.js
+++ beautify.js
@@ -7,6 +7,7 @@ var fs = require('fs');
 var beautify = require('js-beautify').js_beautify;
 var readline = require('readline');
 var mkdirp = require('mkdirp');
+var options = require('./options.json');
 
 program
   .version('0.0.1')
@@ -19,24 +20,6 @@ program
 if(!program.directory) {
   program.help();
 }
-
-var options = {
-  "indent_size": 2,
-  "indent_char": " ",
-  "indent_level": 0,
-  "indent_with_tabs": false,
-  "preserve_newlines": true,
-  "max_preserve_newlines": 2,
-  "jslint_happy": true,
-  "brace_style": "collapse",
-  "keep_array_indentation": false,
-  "keep_function_indentation": false,
-  "space_before_conditional": true,
-  "break_chained_methods": false,
-  "eval_code": false,
-  "unescape_strings": false,
-  "wrap_line_length": 80
-};
 
 var cwd = process.cwd();
 var base = path.join(cwd, program.directory);
